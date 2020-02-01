aad6af1273061c2b9b3a1634cb8950e22598c84f
Canguilhem <clerger.a35@gmail.com>
2020-02-01 15:51:44 +0800

    add PostService

--- services/Postservice.mjs
+++ services/Postservice.mjs
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getPosts(){
     try {
-        return await axios.get('.netlify/functions/ReadPost')
+        return {data} = await axios.get('.netlify/functions/ReadPost')
     } catch (error) {
         console.log('PostService:getPosts', error)
     }
