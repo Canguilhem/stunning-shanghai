b411722226b1c961562b6b279ca74ef71fc07bcd
Canguilhem <clerger.a35@gmail.com>
2020-03-14 18:40:01 +0800

    add sidebar

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,9 +44,9 @@ module.exports = {
     // './assets/css/fontello-embedded.css',
     // './assets/css/fontello-ie7-codes.css', /*if IE 7 */
     // '.:assets/css/fontello-ie7.css',
+    {src:'~/assets/css/fontello.css'},
     {src:'@/assets/css/fontello.css'},
     {src:'./assets/css/fontello.css'},
-    {src:'~/assets/css/fontello.css'},
     
   ],
   /*
