a86a385967b69a34f7198c003c7187f5dbb6512f
Canguilhem <clerger.a35@gmail.com>
2020-03-14 19:08:18 +0800

    css path resolution

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,6 @@ module.exports = {
     // './assets/css/fontello-embedded.css',
     // './assets/css/fontello-ie7-codes.css', /*if IE 7 */
     // '.:assets/css/fontello-ie7.css',
-    {src:'~static/css/fontello.css' , ssr: false},
     {src:'~assets/css/fontello.css'},
     {src:'./assets/css/fontello.css'},
     
