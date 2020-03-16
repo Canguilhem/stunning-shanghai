dfc50252a9a0b2c7c8235996ed73db558592cf73
Canguilhem <clerger.a35@gmail.com>
2020-03-16 18:19:30 +0800

    layout mobile sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,31 +1,41 @@
 <template>
-  <div class="container" :class="{'show': showSidebar}">
+  <div class="container" :class="{'show': showSidebar, 'show--mobile': showMobile }">
+    <div class="mobile--grip" @click="showNavMobile">
+      <div class="animated infinite wobble slower"></div>
+      <div class="animated infinite wobble slow delay-2s"></div>
+      <div class="animated infinite wobble delay-3s"></div>
+    </div>
     <div class="control" @click="showNav">
       <i class="fas fa-angle-double-right"></i>
     </div>
     <div class="navigation-icons">
       <nuxt-link to="/">
-        <i class="fas fa-home"></i>
+        <i class="fas fa-home" v-b-tooltip.hover.right.v-warning title="Home"></i>
         <p v-show="showLink" class="animated faster" :class="{ zoomIn: showLink }">Home</p>
       </nuxt-link>
       <nuxt-link to="/shanghai">
-        <i class="icon-shanghai"></i>
+        <i class="icon-shanghai" v-b-tooltip.hover.right.v-danger title="Stunning Shanghai"></i>
         <p v-show="showLink" class="animated faster" :class="{ zoomIn: showLink }">Stunning Shanghai</p>
       </nuxt-link>
       <nuxt-link to="/youtube">
-        <i class="icon-youtube"></i>
+        <i class="icon-youtube" v-b-tooltip.hover.right.v-success title="Interesting Youtube"></i>
         <p v-show="showLink" class="animated faster" :class="{ zoomIn: showLink }">Interesting Youtube</p>
       </nuxt-link>
       <nuxt-link to="/crypto">
-        <i class="icon-bitcoin"></i>
+        <i class="icon-bitcoin" v-b-tooltip.hover.right.v-info title="Crypto Tracker"></i>
         <p v-show="showLink" class="animated faster" :class="{ zoomIn: showLink }">Crypto Tracker</p>
       </nuxt-link>
       <nuxt-link to="/glossary">
-        <i class="icon-explained"></i>
+        <i class="icon-explained" v-b-tooltip.hover.right.v-primary title="Layman's Blockchain"></i>
+        <p v-show="showLink" class="animated faster" :class="{zoomIn: showLink }">Layman's Blockchain</p>
+      </nuxt-link>
+      <nuxt-link to="/glossary">
+        <i class="icon-explained" v-b-tooltip.hover.right.v-primary title="Layman's Blockchain"></i>
         <p v-show="showLink" class="animated faster" :class="{zoomIn: showLink }">Layman's Blockchain</p>
       </nuxt-link>
     </div>
   </div>
+  <!-- Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> -->
 </template>
 
 <script>
@@ -33,7 +43,8 @@ export default {
   data() {
     return {
       showSidebar: false,
-      showLink: false
+      showLink: false,
+      showMobile: false
     };
   },
   methods: {
@@ -49,6 +60,10 @@ export default {
           this.showLink = true;
         }, 500);
       }
+    },
+    showNavMobile(){
+      console.log("clicked:", this.showMobile)
+      this.showMobile = !this.showMobile
     }
   }
 };
@@ -153,6 +168,28 @@ export default {
    .fa-home{
      margin-top: 16px;
    }
+  }
+}
+@media screen and (max-width: 450px) {
+  .container{
+    width: 30px;
+    position: fixed;
+    top: 150px;
+    .control,.navigation-icons{
+      display: none;
+    }
+    .mobile--grip {
+      display: grid;
+      grid-template-columns: auto;
+      justify-content: center;
+      row-gap: 5px;
+      div{
+        height: 10px;
+        width: 10px;
+        background-color: #fff;
+        border-radius: 50%;
+      }
+    }
   }
 }
 </style>
