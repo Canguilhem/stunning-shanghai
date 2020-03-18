0ab87c3c46a4f824b32a9a7ca85c0e59cd9d47bf
Canguilhem <clerger.a35@gmail.com>
2020-03-18 14:45:20 +0800

    order formations

--- functions/ReadExp.js
+++ functions/ReadExp.js
@@ -6,7 +6,7 @@ exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false
   try {
     const experiences = await Experience.find().sort('-from'),
-    formations = await Formation.find().sort('from'),
+    formations = await Formation.find().sort('-from'),
           response = {
             experiences,
             formations
